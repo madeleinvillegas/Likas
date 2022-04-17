d72bcf7624411e61f593bcfa2a4e9c0b0e470523
ira-renzo <keimayuuki@gmail.com>
2022-04-17 21:49:28 +0800

    Working QR Scanner.

--- app/src/main/java/com/example/likas/MainActivity.java
+++ app/src/main/java/com/example/likas/MainActivity.java
@@ -1,19 +1,19 @@
 package com.example.likas;
 
 import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.likas.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,15 +22,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_locate, R.id.navigation_identify,
-                R.id.navigation_update, R.id.navigation_share)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
-    }
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(navView, navController);
 
+        // Account
+        ImageView accountIcon = findViewById(R.id.account);
+        accountIcon.setOnClickListener(new View.OnClickListener() {
+            int count = 0;
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), String.valueOf(count++), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/likas/ui/tab_02_identify/IdentifyFragment.java
+++ app/src/main/java/com/example/likas/ui/tab_02_identify/IdentifyFragment.java
@@ -1,20 +1,29 @@
 package com.example.likas.ui.tab_02_identify;
 
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
+import com.example.likas.R;
 import com.example.likas.databinding.Tab02IdentifyBinding;
+import com.google.android.material.navigation.NavigationView;
 
-public class IdentifyFragment extends Fragment {
+import java.util.Objects;
+
+public class IdentifyFragment extends Fragment implements QRCodeReaderView.OnQRCodeReadListener {
 
     private Tab02IdentifyBinding binding;
+    @SuppressWarnings("unused")
+    private QRCodeReaderView qrCodeReaderView;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         IdentifyViewModel locateViewModel = new ViewModelProvider(this).get(IdentifyViewModel.class);
@@ -24,6 +33,15 @@ public class IdentifyFragment extends Fragment {
 
         final TextView textView = binding.textIdentify;
         locateViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        // QR
+        qrCodeReaderView = binding.qrdecoderview;
+        qrCodeReaderView.setOnQRCodeReadListener(this);
+        qrCodeReaderView.setQRDecodingEnabled(true);
+        qrCodeReaderView.setAutofocusInterval(2000L);
+        qrCodeReaderView.setTorchEnabled(true);
+        qrCodeReaderView.setBackCamera();
+
         return root;
     }
 
@@ -31,5 +49,22 @@ public class IdentifyFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void onQRCodeRead(String text, PointF[] points) {
+        binding.textIdentify.setText(text);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        qrCodeReaderView.startCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        qrCodeReaderView.stopCamera();
     }
 }
